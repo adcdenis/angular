ab82e9060ad08fc1444cc654cd45c2aa6b01cf74
Antonio Denilson Canuto <adcdenis@gmail.com>
2022-08-26 15:36:24 -0300

    inicio estudo jwt auth, com fake jwt

---
+++ curso-de-angular-auth-front-main/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+// Guard
+import { AuthGuard } from './core/guard/auth.guard';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./core/components/auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'admin',
+    canActivateChild: [AuthGuard],
+    loadChildren: () =>
+      import('./components/admin/admin.module').then((m) => m.AdminModule),
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
