ab80090370700d14fddc54c5216d9c7960ab7895
Antonio <adcdenis@gmail.com>
2022-08-27 01:15:49 -0300

    criação tela de login

--- auth-curso/src/app/components/admin/admin.module.ts
+++ auth-curso/src/app/components/admin/admin.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { HomeComponent } from './pages/home/home.component';
+import { AdminRoutingModule } from './admin-routing.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule, AdminRoutingModule
   ]
 })
 export class AdminModule { }
---
+++ auth-curso/src/app/core/components/auth/auth-routing.module.ts
@@ -0,0 +1,17 @@
+import { SignComponent } from './pages/sign/sign.component';
+import { HomeComponent } from './../../../components/admin/pages/home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: SignComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class AuthRoutingModule {}
