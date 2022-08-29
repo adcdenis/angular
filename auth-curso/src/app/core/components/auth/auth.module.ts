71da3559866bafb971ab3d223987ffb16c41c349
Antonio <adcdenis@gmail.com>
2022-08-29 00:38:44 -0300

    mistério ainda

--- auth-curso/src/app/core/components/auth/auth.module.ts
+++ auth-curso/src/app/core/components/auth/auth.module.ts
@@ -1,21 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { AuthRoutingModule } from './auth-routing.module';
-import { SignComponent } from './pages/sign/sign.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
+// Routing
+import { AuthRoutingModule } from './auth-routing.module';
+
+// Pages Component
+import { SignComponent } from './pages/sign/sign.component';
 
 @NgModule({
-  declarations: [
-    SignComponent
-  ],
-  imports: [
-    CommonModule,
-    AuthRoutingModule,
-    ReactiveFormsModule,
-    HttpClientModule
-  ]
+  declarations: [SignComponent],
+  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule],
 })
-export class AuthModule { }
+export class AuthModule {}
--- auth-curso/src/app/core/components/auth/pages/sign/sign.component.ts
+++ auth-curso/src/app/core/components/auth/pages/sign/sign.component.ts
@@ -7,19 +7,30 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./sign.component.scss'],
 })
 export class SignComponent implements OnInit {
-  constructor(private formBuilder: FormBuilder) {}
-
   public formAuth: FormGroup = this.formBuilder.group({
-    email: ['', [Validators.email, Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required]],
   });
 
+  public msgError!: string;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {}
+
   ngOnInit(): void {}
 
-  public submit() {
-    console.log();
-    if (this.formAuth.valid) {
-      console.log();
-    }
+  public submitForm() {
+    // if (this.formAuth.valid) {
+    //   this.authService
+    //     .sign({
+    //       email: this.formAuth.value.email,
+    //       password: this.formAuth.value.password,
+    //     })
+    //     .subscribe({
+    //       next: (res) => res,
+    //       error: (e) => (this.msgError = e),
+    //     });
+    // }
   }
 }
