30636526a355488c871b3239e0084d90a76ff73a
Antonio <adcdenis@gmail.com>
2022-09-02 01:52:19 -0300

    - iniciando curso typescript

--- curso-javascript/sintaxe/index.js
+++ curso-javascript/sintaxe/index.js
@@ -1,2 +1,28 @@
-const teste = 'ui';
-console.log(teste);
+const teste = 'funciona javascript';
+console.log(String(111));
+
+console.log("1"+2);
+console.log("1"-2);
+console.log("1"*2);
+console.log("1"/2);
+console.log("1"+"2");
+
+console.log('-------------------------------------------------------');
+console.log(typeof 1);
+console.log(typeof "1");
+console.log(typeof Number('2'));
+console.log(typeof String(3333));
+console.log(typeof NaN);
+
+console.log('-------------------------------------------------------');
+
+
+console.log({nome:String,telefone:Number});
+let nome;
+let telefone;
+console.log({nome,telefone});
+
+const lista = ["Antonio", "Denilson", "Canuto"]
+lista.forEach(
+    (item, index) => console.log(`${item} - ${index}`)
+)
